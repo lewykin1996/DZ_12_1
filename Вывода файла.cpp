c650aa192a62a068119217b1b37998a55281a6f8
lewykin1996 <lewykin1996@gmail.com>
2026-02-01 12:29:01 +0300

    Add files via upload

---
+++ Вывода файла.cpp
@@ -0,0 +1,28 @@
+﻿// Вывода файла.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	//setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::ifstream ifile{ "in.txt" };
+
+	std::string data;
+
+	while (ifile >> data)
+	{
+		std::cout << data << std::endl;
+	}
+
+	ifile.close();
+
+	return 0;
+}
